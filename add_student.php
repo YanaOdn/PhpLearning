d0ee31b5da588af5d3a5bbce8934d3251722d9d4
YanaOdn <yaodnokonnaya@gmail.com>
2016-02-14 22:02:33 +0300

    Поправили форматирование в форме

--- add_student.php
+++ add_student.php
@@ -4,12 +4,11 @@
     <title>Форма добавления студентов</title>
 </head>
 <body>
-    <form action = '/worker.php' method="get">
-         <input type = text name = surname placeholder="Фамилия"> <br>
-         <input type = text name = name placeholder="Имя"> <br>
-         <input type = text name = middle_name placeholder="Отчество"> <br>
-        <input type = submit value= "добавить студента">
-
+    <form action='/worker.php' method="get">
+        <input type=text name=surname placeholder="Фамилия"> <br>
+        <input type=text name=name placeholder="Имя"> <br>
+        <input type=text name=middle_name placeholder="Отчество"> <br>
+        <input type=submit value="Добавить студента">
     </form>
 </body>
 </html>
