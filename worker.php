ebd86a3026c0f84a8a6a5c55f3fd2ef4c268422e
yana <yaodnokonnaya@gmail.com>
2016-03-10 23:45:18 +0300

    saving form in db

--- worker.php
+++ worker.php
@@ -1,12 +1,22 @@
 <?php
 
 $surname = $_GET ["surname"];
-
+$name = $_GET ["name"];
+$middle_name = $_GET ["middle_name"];
+$birthday = $_GET ["birthday"];
+$sex = $_GET ["sex"];
+$year = $_GET ["year"];
+$language = $_GET ["language"];
+$comment = $_GET ["comment"];
         $connect = mysql_connect("localhost", 'root', 123);
 
         mysql_select_db('asdfasdf');
 
-        $query = "insert into student (name) value ('$surname')";
+        $query = "
+                insert into
+                student (surname, name, middle_name, birthday, sex, year, language, comment)
+                value ('$surname', '$name', '$middle_name', '$birthday', '$sex', '$year', '$language', '$comment' )
+        ";
 
         $result = mysql_query($query);
 
